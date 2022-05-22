e2632b65381fe9befe174a4538404fb290c2e652
vspanda <hc.howard.hui@gmail.com>
2022-05-22 18:32:32 +0800

    basic min y ai

--- Player/AlphaTetris.py
+++ Player/AlphaTetris.py
@@ -2,14 +2,48 @@
 PKUDSA Tetris ALPHA Team AI
 2022.5.22
 
+Allowed Libraries:
+- math
+- random
+- copy
+- time
+- collections
+- itertools
+- functools
+- operator
+- numpy
+
+Time Limit per Turn: 9999
+
+First Thoughts: just put piece in lowest place possible
 """
 
-import math, random, copy, time, collections, itertools, functools, operator, numpy
 
 
-class Player:
-    def __init__(self, isFirst: bool) -> None:
+class Player:  
+    def __init__(self, isFirst):
         self.isFirst = isFirst
 
-    def output(self, matchData) -> tuple(int, int, int):
-        return
+        self.comp = min if isFirst else max
+        
+        self.initialized = False
+
+        self.blocks = None
+        self.blockNum = None
+        
+
+    def output(self, matchData):
+        if not self.initialized:
+            self.blocks = list(matchData.getBlockList())
+            self.blockNum = 1 if self.isFirst else 2
+            self.initialized = True
+        
+        board = matchData.getBoard()
+        block = self.blocks[self.blockNum]
+
+        validMoves = matchData.getAllValidAction(block, board)
+        
+
+        move = self.comp(validMoves, key=lambda x:x[0])
+        self.blockNum += 2
+        return move
