1048b48ca43e63f38cf5711ae492f7e608c6f906
vspanda <hc.howard.hui@gmail.com>
2022-05-22 16:10:13 +0800

    added some comments

--- Player/AlphaTetris.py
+++ Player/AlphaTetris.py
@@ -1,7 +1,8 @@
-class Tuple:
-    pass
-class MatchData:
-    pass
+"""
+PKUDSA Tetris ALPHA Team AI
+2022.5.22
+
+"""
 
 import math, random, copy, time, collections, itertools, functools, operator, numpy
 
@@ -10,5 +11,5 @@ class Player:
     def __init__(self, isFirst: bool) -> None:
         self.isFirst = isFirst
 
-    def output(self, matchData: MatchData) -> Tuple[int, int, int]:
+    def output(self, matchData) -> tuple(int, int, int):
         return
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Team ALPHA for pkudsa.tetris 2022
+
 Start Date: 2022.5.22
