3215f3b87f82cc8cb3f23f7e6d03c43b92c16a3d
vspanda <hc.howard.hui@gmail.com>
2022-05-22 19:29:42 +0800

    most Lines

--- Player/AlphaTetris.py
+++ Player/AlphaTetris.py
@@ -18,33 +18,40 @@ Time Limit per Turn: 9999
 First Thoughts: just put piece in lowest place possible
 """
 
-
-class Player:  
+class Player:
     def __init__(self, isFirst):
         self.isFirst = isFirst
 
-        self.comp = min if isFirst else max
-        self.less = lambda x:x[0] < x[1] if self.isFirst else \
-            lambda x:x[1] < x[0]
-        
+        self.comp = max
+
         self.initialized = False
 
         self.blocks = None
         self.blockNum = None
-        
+
+    def linesFull(self, board):
+        f = 0
+        for line in board:
+            if 0 not in line:
+                f += 1
+        return f   
 
     def output(self, matchData):
         if not self.initialized:
             self.blocks = list(matchData.getBlockList())
             self.blockNum = 1 if self.isFirst else 2
             self.initialized = True
-        
+
         board = matchData.getBoard()
         block = self.blocks[self.blockNum]
 
         validMoves = matchData.getAllValidAction(block, board)
-        
 
-        move = self.comp(validMoves, key=self.less)
+        scores = []
+        for move in validMoves:
+            testBoard = board[:]
+            matchData.putBlock(block, move, testBoard)
+            scores.append(self.linesFull(testBoard))
+
         self.blockNum += 2
-        return move
+        return validMoves[max(scores)]
