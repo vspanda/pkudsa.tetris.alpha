2997b10a5536e982e9f8e0e5283c4d05b5d3bf8f
vspanda <hc.howard.hui@gmail.com>
2022-05-22 18:48:21 +0800

    small update so it works lol

--- Player/AlphaTetris.py
+++ Player/AlphaTetris.py
@@ -19,28 +19,32 @@ First Thoughts: just put piece in lowest place possible
 """
 
 
-class Player:
+class Player:  
     def __init__(self, isFirst):
         self.isFirst = isFirst
 
         self.comp = min if isFirst else max
-
+        self.less = lambda x:x[0] < x[1] if self.isFirst else \
+            lambda x:x[1] < x[0]
+        
         self.initialized = False
 
         self.blocks = None
         self.blockNum = None
+        
 
     def output(self, matchData):
         if not self.initialized:
             self.blocks = list(matchData.getBlockList())
             self.blockNum = 1 if self.isFirst else 2
             self.initialized = True
-
+        
         board = matchData.getBoard()
         block = self.blocks[self.blockNum]
 
         validMoves = matchData.getAllValidAction(block, board)
+        
 
-        move = self.comp(validMoves, key=lambda x: x[0])
+        move = self.comp(validMoves, key=self.less)
         self.blockNum += 2
         return move
