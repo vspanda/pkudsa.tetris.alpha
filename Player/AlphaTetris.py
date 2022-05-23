d56a64a126b693ae322c433e3efaf99536685f9f
vspanda <hc.howard.hui@gmail.com>
2022-05-23 21:30:20 +0800

    added Aggregate height calculation

--- Player/AlphaTetris.py
+++ Player/AlphaTetris.py
@@ -13,50 +13,95 @@ Allowed Libraries:
 - operator
 - numpy
 
-Time Limit per Turn: 9999
+Time Limit: 9999
 
 First Thoughts: just put piece in lowest place possible
 """
+
 
 class Player:
     def __init__(self, isFirst):
         self.isFirst = isFirst
 
-        self.comp = max
-
         self.initialized = False
-
         self.blocks = None
-        self.blockNum = None   
+        self.blockNum = None
 
     def output(self, matchData):
-        
-        def copyBoard(board):
-            return list(map(list, board))
-
-        def linesFull(board):
-            f = 0
-            for line in board:
-                if 0 not in line:
-                    f += 1
-            return f   
-
+        # Redo Board with NUMPY
         if not self.initialized:
             self.blocks = list(matchData.getBlockList())
             self.blockNum = 1 if self.isFirst else 2
             self.initialized = True
 
+        def copyBoard(board):
+            return list(map(list, board))
+
+        def linesFull(board):
+            full = 0
+            for line in board:
+                if 0 not in line:
+                    full += 1
+            return full
+
+        def holesFound(board):
+            holes = 0
+
+            return holes
+
+        def aggregateHeight(board):
+            if self.isFirst:
+                base = 15
+                colRange = range(15)
+                getHeight = lambda x: base - x
+                pass
+            else:
+                base = 10
+                colRange = range(24, 9, -1)
+                getHeight = lambda x: x - base
+                pass
+
+            def getColumnHeight(col):
+                for i in colRange:
+                    if board[i][col]:
+                        return getHeight(i)
+                return 0
+
+            height = 0
+            for i in range(10):
+                height += getColumnHeight(i)
+
+            return height
+
+        def bumpiness(board):
+            bumpiness = 0
+            return bumpiness
+
+        def villainScore(board):
+            vScore = 0
+            return vScore
+
         board = matchData.getBoard()
-        block = self.blocks[self.blockNum]
+        if self.blockNum < len(self.blocks):
+            block = self.blocks[self.blockNum]
 
         validMoves = matchData.getAllValidAction(block, board)
 
         scores = []
         for move in validMoves:
             testBoard = copyBoard(board)
+
+            # Can use Numpy to reimplement
             matchData.putBlock(block, move, testBoard)
-            scores.append(linesFull(testBoard))
-
+            scores.append(
+                0
+                + 750 * linesFull(testBoard)
+                - 300 * holesFound(testBoard)
+                - 500 * aggregateHeight(testBoard)
+                - 200 * bumpiness(testBoard)
+                - 300 * villainScore(testBoard)
+            )
         self.blockNum += 2
-        return validMoves[max(scores)]
-
+        if validMoves == []:
+            return (0, 0, 0)
+        return validMoves[scores.index(max(scores))]
